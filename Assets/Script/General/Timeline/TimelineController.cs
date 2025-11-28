eb89afb39a8a4b198a2ee9aaca60e8dc32b94c7e
ShutongZhang2023 <125089924+ShutongZhang2023@users.noreply.github.com>
2025-11-28 11:36:30 -0500

    timeline basic (without highlight)

--- Assets/Script/General/Timeline/TimelineController.cs
+++ Assets/Script/General/Timeline/TimelineController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +10,14 @@ public class TimelineController : MonoBehaviour
     private void OnEnable()
     {
         LoadNodes();
-        RebuildTimeline();
+        StartCoroutine(DelayedRebuild());
+    }
+
+    private IEnumerator DelayedRebuild()
+    {
+        yield return null;   // 等 1 帧，确保 RectTransform 和 Canvas 布局稳定
+
+        RebuildTimeline();   // 现在重建线不会出现任何 null / 坐标错误
     }
 
     private void LoadNodes()
@@ -29,6 +37,7 @@ public class TimelineController : MonoBehaviour
     {
         foreach (var node in nodes)
         {
+            if (node == null) continue;
             node.RefreshVisibility();
         }
 
