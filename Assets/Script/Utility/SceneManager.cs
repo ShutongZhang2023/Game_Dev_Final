bef92384bb366d3b45e91832f2081b9edad9bbf5
LucasQ520 <lucasqiao520@gmail.com>
2025-12-01 03:47:18 -0500

    TimelineToSceneTransition

---
+++ Assets/Script/Utility/SceneManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager instance;
+    public string persistentSceneName = "Presist";
+    public string firstSceneName = "Scene1";
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    public void ChangeContentScene(string sceneName)
+    {
+        StartCoroutine(LoadNewSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadNewSceneAsync(string newSceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Additive);
+
+        yield return null;
+
+        for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
+        {
+            Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
+            if (scene.isLoaded && scene.name != persistentSceneName && scene.name != newSceneName)
+            {
+                yield return UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(scene);
+            }
+        }
+    }
+}
