274da0dc89aa7cf6553e3e642d472fcc723e9271
Sharky <fangshenghan720@126.com>
2025-12-07 22:04:03 -0500

    changed coffee scene sprites

--- Assets/Script/General/Scene4/CoffeeCup.cs
+++ Assets/Script/General/Scene4/CoffeeCup.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,11 +7,15 @@ public class CoffeeCup : Clickable
 {
 
     public GameObject choicePanel;
+    public SpriteRenderer secondBackgroundSpriteRenderer;
 
     protected override void OnClicked()
     {
         base.OnClicked();
         choicePanel.SetActive(true);
+        secondBackgroundSpriteRenderer.DOColor(new Color(1, 1, 1, 1), 1f);
+
+        gameObject.SetActive(false);
     }
 
 }
